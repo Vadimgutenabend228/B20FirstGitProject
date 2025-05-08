3a8dc028cb912ef37b6ea9f1382e2289dbc23928
Blagoja <blagojaan@yahoo.com>
2025-05-08 20:26:24 +0200

    2. Test wurde geschrieben

--- src/test/java/com/eurotech/tests/LoginTest.java
+++ src/test/java/com/eurotech/tests/LoginTest.java
@@ -4,6 +4,7 @@ public class LoginTest {
 
     public static void main(String[] args) {
         System.out.println("Erster Test wurde geschrieben");
+        System.out.println("2. Test wurde geschrieben");
 
 
 
